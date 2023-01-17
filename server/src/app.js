c249e96190881a670e6ab110e28ed983e7b0e878
Manuel Serafin Bugarin <elser.code@gmail.com>
2023-01-17 15:05:44 +0800

    Adde launchces and server files

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+const { planetsRouter } = require("./routes/planets/planets.router");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+module.exports = app;
