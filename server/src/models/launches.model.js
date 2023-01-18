1e524b8f58a7ef133a0723b54393e1ee500bd55a
Manuel Serafin Bugarin <elser.code@gmail.com>
2023-01-18 17:28:46 +0800

    Added launch mission

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   customer: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
@@ -17,4 +19,17 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      customers: ["Zero to Mastery", "NASA"],
+      upcoming: true,
+      success: true,
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
